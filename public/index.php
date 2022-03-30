a05fa8a4ec9d8b4cefd58eba134921af94a9c571
Robin Delbaere <hello@rdelbaere.fr>
2022-03-30 12:32:55 +0200

    Form with label & button

--- public/index.php
+++ public/index.php
@@ -7,24 +7,31 @@ $template = '<form>';
 foreach ($formConfig->form->fields as $field) {
     $template .= '<div>';
 
+    if(isset($field->label)){
+        $template .= '<label for="'.$field->name.'">'.$field->label.'</label>';
+    }
+
     switch ($field->type){
         case 'text':
-            $template .= '<input type="text" name="'.$field->name.'">';
+            $template .= '<input type="text" id="'.$field->name.'" name="'.$field->name.'">';
             break;
         case 'textarea':
-            $template .= '<textarea name="'.$field->name.'"></textarea>';
+            $template .= '<textarea name="'.$field->name.'" id="'.$field->name.'"></textarea>';
             break;
         case 'checkbox':
-            $template .= '<input type="checkbox" name="'.$field->name.'">';
+            $template .= '<input type="checkbox" name="'.$field->name.'" id="'.$field->name.'">';
             break;
         case 'select':
-            $template .= '<select name="'.$field->name.'">';
+            $template .= '<select name="'.$field->name.'" id="'.$field->name.'">';
 
             foreach ($field->values as $value) {
                 $template .= '<option value="'.$value->value.'">'.$value->text.'</option>';
             }
 
             $template .= '</select>';
+            break;
+        case 'submit':
+            $template .= '<button type="submit">'.$field->text.'</button>';
             break;
     }
 
